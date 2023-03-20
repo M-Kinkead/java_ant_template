b3ecfb6caf9531a8ba8cf9cbdc1f9c5da08d8e06
MKinkead <MKinkead@M2L116TC01.belfastmet.int>
2023-03-20 09:58:30 +0000

    Version 1

---
+++ src/java_ant_template/Java_ant_template.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java_ant_template;
+
+/**
+ *
+ * @author MKinkead
+ */
+public class Java_ant_template {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // This is your main method!
+    }
+    
+}
